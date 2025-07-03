dbf5b777cf9d289c724c23e98354fff024964fcc
Brandon Kim <brandon.kim71@gmail.com>
2025-07-03 15:55:24 +0900

    Quantum KOREA 2025 관련 콘텐츠 추가 및 이미지 파일 업로드

---
+++ assets/images/quantum_korea_2025/create_th.bat
@@ -0,0 +1,5 @@
+D:\utils\ImageMagick-7.1.1-Q16-HDRI\convert ./HZ1_1459.jpg -resize 300x200 HZ1_1459_th.png
+D:\utils\ImageMagick-7.1.1-Q16-HDRI\convert ./HZ1_1464.jpg -resize 300x200 HZ1_1464_th.png
+D:\utils\ImageMagick-7.1.1-Q16-HDRI\convert ./HZ1_2076.jpg -resize 300x200 HZ1_2076_th.png
+D:\utils\ImageMagick-7.1.1-Q16-HDRI\convert ./HZ1_2843.jpg -resize 300x200 HZ1_2843_th.png
+D:\utils\ImageMagick-7.1.1-Q16-HDRI\convert ./HZ1_5012.jpg -resize 300x200 HZ1_5012_th.png
